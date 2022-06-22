7c552d96257456f581b15a2943beff103b303bb7
nicocribioli <nicolas-cribioli@hotmail.com>
2022-06-22 00:27:49 -0300

    Updates

---
+++ precache-manifest.459172cbd927e3bdc216699c4e72564b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dfcc2f8005da7c40ced",
+    "url": "/calculator/static/css/main.f0f38e8e.chunk.css"
+  },
+  {
+    "revision": "6dfcc2f8005da7c40ced",
+    "url": "/calculator/static/js/main.6dfcc2f8.chunk.js"
+  },
+  {
+    "revision": "9afc0394e64fe1297f2c",
+    "url": "/calculator/static/js/1.9afc0394.chunk.js"
+  },
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "ea1e12fa1267e95053989d1de22e2a7d",
+    "url": "/calculator/static/media/calculator-img.ea1e12fa.png"
+  },
+  {
+    "revision": "65fdc8dbc6e20a570632accce6d795a4",
+    "url": "/calculator/index.html"
+  }
+];
